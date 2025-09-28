f23eb10170cb44d98ae7b64571fe6c3eff6cc18e
Julian Skifstad <juliansk@umich.edu>
2025-09-28 19:15:39 -0400

    lqr working

---
+++ lqrgpt.py
@@ -0,0 +1,118 @@
+import torch as th
+from lpe.lpe.utils import Transformer
+import matplotlib.pyplot as plt
+from functools import partial
+import lqr_utils as lqr
+
+model_name = "gelu-4l"
+device = th.device("cuda" if th.cuda.is_available() else "cpu")
+model = Transformer.from_pretrained(model_name).to(device)
+
+tfs_raw = model.blocks
+tfs_with_control = [partial(lqr.transformerBlockControl, tf) for tf in tfs_raw]
+
+n =  model.embed.d_model
+m = n
+T = len(tfs_with_control)
+
+
+input = th.tensor([32990])
+# input = th.tensor([10000])
+
+# Time and stage parameters -- do we care about anything other than N?
+
+
+U_nom = th.zeros((T, m), device=device)
+X_nom = th.zeros((T+1, n), device=device)
+# X_nom[0] = x + model.pos_embed(input)
+# print(f"X_nom: {X_nom[0]}")
+# for i in range(1, T):
+#     X_nom[i] = tfs_raw[i-1](X_nom[i-1])
+
+## Generate target
+random_input = th.tensor([33990]) # WORKS for 4layer
+# random_input = th.tensor([36990])
+onehot = th.nn.functional.one_hot(random_input, num_classes=model.embed.d_vocab).float().to(device)
+onehot.requires_grad_(True)
+x_tar = onehot @ model.embed.W_E
+x_tar = x_tar + model.pos_embed(input)
+
+X_nom[0] = x_tar
+X_nom[T] = lqr.find_random_target(model,x_tar)
+for i in range(1, T):
+    X_nom[i] = tfs_raw[i-1](X_nom[i-1])
+# diff = (X_nom[T] - X_nom[0]) / (float)(T)
+# for i in range(1, T):
+#     X_nom[i] = X_nom[i-1] + diff
+
+# print(X_nom.shape)
+
+x_lnfinal = model.ln_final(X_nom[T].unsqueeze(0).unsqueeze(0))
+# print(x.shape)
+logits = model.unembed(x_lnfinal).squeeze(1)
+# print(logits.shape)
+
+target = logits.argmax(-1)
+print(f"target: {target}")
+
+## Linearize dynamics around nominal
+A, B = lqr.linearize(tfs_with_control,T,m,X_nom)
+
+# # Define quadratic cost matrices
+Q = th.eye(n).unsqueeze(0).repeat(T, 1, 1).to(A.device) * 1
+R = th.eye(m).unsqueeze(0).repeat(T, 1, 1).to(A.device) * 1
+Qf = 10000 * th.eye(n).to(A.device)
+
+# Solve LQR on linearized system
+K = lqr.time_varying_lqr(A, B, Q, R, Qf)
+
+# print("Feedback gains K shape:", K.shape)
+# print("K[0]:", K[0])
+
+X = th.zeros_like(X_nom)
+U = th.zeros_like(U_nom)
+
+onehot = th.nn.functional.one_hot(input, num_classes=model.embed.d_vocab).float().to(device)
+onehot.requires_grad_(True)
+x = onehot @ model.embed.W_E
+
+
+X[0] = x + model.pos_embed(input)
+# X[0] = X_nom[0]
+for i in range(T):
+    U[i] = U_nom[i]-K[i]@(X[i]-X_nom[i])
+    X[i+1] = tfs_with_control[i](X[i], U[i])
+
+# print(f"U[0]: {U[0]}")
+# print(f"U[1]: {U[1]}")
+# print(f"U[-1]: {U[-1]}")
+
+x = model.ln_final(X[T].unsqueeze(0).unsqueeze(0))
+logits = model.unembed(x).squeeze(1)
+target = logits.argmax(-1)
+print(f"found: {target}")
+
+print("VERIFICATION:")
+input = th.tensor([32990])
+
+onehot = th.nn.functional.one_hot(input, num_classes=model.embed.d_vocab).float().to(device)
+onehot.requires_grad_(True)
+x_p = onehot @ model.embed.W_E
+x_p = x_p + model.pos_embed(input)
+for block in model.blocks:
+    x_p = block(x_p)
+x_p = model.ln_final(x_p[:,-1].unsqueeze(1))
+logits = model.unembed(x_p).squeeze(1)
+y = logits.argmax(-1)
+
+print(f"output no steering = {y}")
+
+x_s = onehot @ model.embed.W_E
+x_s = x_s + model.pos_embed(input)
+for i, block in enumerate(model.blocks):
+    x_s = block(x_s) + U[i]
+x_s = model.ln_final(x_s[:,-1].unsqueeze(1))
+logits = model.unembed(x_s).squeeze(1)
+y = logits.argmax(-1)
+
+print(f"output with steering = {y}")
